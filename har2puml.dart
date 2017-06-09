dfeaeb0b38d9f41f2c03b460f54c6871bca415b7
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2017-06-09 15:32:58 +0300

    format

--- har2puml.dart
+++ har2puml.dart
@@ -7,31 +7,34 @@ const client = 'browser';
 void main() {
   var servers = new LinkedHashMap();
 
-  stdin
-    .transform(UTF8.decoder)
-    .transform(JSON.decoder)
-    .listen((har) {
-      har['log']['entries'].forEach((e) {
-        var url = e['request']['url'];
-        var uri = Uri.parse(url);
+  stdin.transform(UTF8.decoder).transform(JSON.decoder).listen((har) {
+    har['log']['entries'].forEach((e) {
+      var url = e['request']['url'];
+      var uri = Uri.parse(url);
 
-        var prefix = servers.keys.firstWhere((key) => url.startsWith(key), orElse:() {
-          var pathIndex = uri.path.indexOf('/', 1);
-          var servlet = pathIndex == -1 ? uri.path.substring(1) : uri.path.substring(1, pathIndex);
-          var newRequest = pathIndex == -1 ? '/' : uri.path.substring(pathIndex);
+      var prefix =
+          servers.keys.firstWhere((key) => url.startsWith(key), orElse: () {
+        var pathIndex = uri.path.indexOf('/', 1);
+        var servlet = pathIndex == -1
+            ? uri.path.substring(1)
+            : uri.path.substring(1, pathIndex);
+        var newRequest = pathIndex == -1 ? '/' : uri.path.substring(pathIndex);
 
-          var prefix = (newRequest == '/') ? url : url.substring(0, 1 + url.indexOf(newRequest));
+        var prefix = (newRequest == '/')
+            ? url
+            : url.substring(0, 1 + url.indexOf(newRequest));
 
-          servers[prefix] = servlet;
- 
-          return prefix;
-        });
+        servers[prefix] = servlet;
 
-        var server = servers[prefix];
-        var request = uri.hasQuery ? url.substring(prefix.length, url.indexOf('?')) : url.substring(prefix.length);      
-
-        stdout.writeln('$client -> "$server" : $request');
+        return prefix;
       });
-    });
-}
 
+      var server = servers[prefix];
+      var request = uri.hasQuery
+          ? url.substring(prefix.length, url.indexOf('?'))
+          : url.substring(prefix.length);
+
+      stdout.writeln('$client -> "$server" : $request');
+    });
+  });
+}
